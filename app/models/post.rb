cd6b89cb15e6dcbfb1e5d0fdba2be78c77da0d25
James Diefenderfer <jimmydief@gmail.com>
2013-06-13 02:01:16 -0700

    POST - replace display_text with built-in rails functionality
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :author, :text, :title, :publish_date
   has_many :comments
   
-  def display_text
-    self.text.gsub(/\n/, '</p><p>').html_safe
-  end
-  
   def display_created_date
     self.created_at.strftime("%B #{ActiveSupport::Inflector.ordinalize(self.created_at.day)}, %Y at %-I:%M %P")
   end
