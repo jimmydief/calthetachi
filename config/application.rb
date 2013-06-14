68c1c69aba043573461ca68637de79c64b5a49cd
James Diefenderfer <jimmydief@gmail.com>
2013-06-14 00:35:17 -0700

    APP - force compile later
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,7 @@ module Calthetachi
     config.active_support.escape_html_entities_in_json = true
     
     config.assets.initialize_on_precompile = false
+    config.assets.compile = true
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
