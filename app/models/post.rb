9573323ffc6f201f4f227dbed4934b09a4df0f24
James Diefenderfer <jimmydief@gmail.com>
2013-06-13 20:29:28 -0700

    POST MODEL - comments depend on post
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :author, :text, :title, :publish_date
-  has_many :comments
+  has_many :comments, :dependent => :destroy 
   
   def display_created_date
     self.created_at.strftime("%B #{ActiveSupport::Inflector.ordinalize(self.created_at.day)}, %Y at %-I:%M %P")
