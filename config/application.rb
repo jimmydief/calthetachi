08a4816d0273f8292071b33f348a5dee729ee129
James Diefenderfer <jimmydief@gmail.com>
2013-06-14 00:56:05 -0700

    PRECOMPILE JS
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -42,7 +42,7 @@ module Calthetachi
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
     
-    config.assets.precompile += %w( *.js *.css )
+    config.assets.precompile += %w( *.js )
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
