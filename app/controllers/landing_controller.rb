f27c8073ef8679ff370c4de918576b36fc52df03
James Diefenderfer <jimmydief@gmail.com>
2013-07-02 02:57:04 -0400

    STATIC PAGES - add some placeholder static pages for contact/about/rush
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,30 @@
 class LandingController < ApplicationController
 
+  # Homepage
   def index
   
     @posts = Post.order("created_at DESC").limit(3)
+  
+  end
+  
+  # About Us
+  def about
+  
+    # Static
+  
+  end
+  
+  # Rush
+  def rush
+  
+    # Static
+  
+  end
+  
+  # Contact Us
+  def contact
+  
+    # Static
   
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Calthetachi::Application.routes.draw do
 
   root :to => "landing#index"
   
+  match "about" => "landing#about", :as => "about_us"
+  match "rush" => "landing#rush", :as => "rush"
+  match "contact" => "landing#contact", :as => "contact"
+  
   resources :posts, :path => "news" do
     resources :comments, :only => [:create, :destroy]
   end
