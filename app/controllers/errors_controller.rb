c0844b0d95d5ece586f030f0e5239d92cf028024
James Diefenderfer <jimmydief@gmail.com>
2013-07-09 19:00:52 -0400

    ERRORS - set and show error messages
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,18 @@
 class ErrorsController < ApplicationController
+  before_filter :set_exception
+
   def not_found
-    
+    @error = env["action_dispatch.exception"]
   end
 
   def internal
-    # Static
+    
   end
+  
+  protected
+ 
+  def set_exception
+    @error = env["action_dispatch.exception"]
+  end
+  
 end
