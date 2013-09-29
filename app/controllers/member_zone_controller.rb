17bdbbe880ec8b227e576f063459935a301f3fa2
James Diefenderfer <jimmydief@gmail.com>
2013-09-28 18:24:03 -0700

    MEMBER ZONE - change password
    Signed-off-by: James Diefenderfer <jimmydief@gmail.com>

--- app/controllers/member_zone_controller.rb
+++ app/controllers/member_zone_controller.rb
@@ -1,5 +1,5 @@
 class MemberZoneController < ApplicationController
-  http_basic_authenticate_with :name => "calthetachi", :password => "arthurchase"
+  http_basic_authenticate_with :name => "calthetachi", :password => "arthurcha$e56"
 
   def index
   
